2f9f482a7800edd8ba477479a81eb8d7a2cb6939
suleymanovtat <suleymanovtat@gmail.com>
2018-04-01 12:37:35 +0300

    created project Data Binding

---
+++ app/src/main/java/ru/suleymanovtat/databindingapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package ru.suleymanovtat.databindingapplication;
+
+import android.databinding.BindingAdapter;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import ru.suleymanovtat.databindingapplication.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @BindingAdapter("app:imageUrl")
+    public static void imageUrl(ImageView imImage, String linkImage) {
+        Picasso.get().load(linkImage).into(imImage);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setUser(new User("ILMIR", "https://i.pinimg.com/originals/8a/f7/b9/8af7b98580255f5005b8891ba3acd696.png"));
+    }
+}
